9392638f24a9be3175cfffb41b3cbf74d976c68f
tadegenban <tadegenban@gmail.com>
2014-12-21 23:37:20 +0800

    export distance

--- lib/Text/Levenshtein/Inline.pm
+++ lib/Text/Levenshtein/Inline.pm
@@ -1,7 +1,13 @@
 package Text::Levenshtein::Inline;
 
 use Text::Levenshtein::Inline::Inline C;
+use base 'Exporter';
 
+our @EXPORT_OK = qw( distance );
 
 __DATA__
 __C__
+
+int distance(char* s, char* t) {
+
+}
