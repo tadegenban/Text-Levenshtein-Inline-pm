421e0d4a1649680e90fd4095cd1df63518fd702e
tadegenban <tadegenban@gmail.com>
2014-12-21 23:48:33 +0800

    update makefile.pl

--- Makefile.pl
+++ Makefile.pl
@@ -1,12 +1,21 @@
+use 5.008001; use strict; use warnings; use utf8;
+
 use lib 'inc';
 use ExtUtils::MakeMaker;
 use Inline::Module;
 
 WriteMakefile(
-  NAME => 'Text::Levenshtein::Inline',
-  postamble => {
-    inline => {
-      module => 'Text::Levenshtein::Inline',
+    NAME => 'Text::Levenshtein::Inline',
+    ABSTRACT => 'Levenshtein distance implement by Inline::C',
+    VERSION_FROM => 'lib/Text/Levenshtein/Inline.pm',
+    AUTHOR => 'tadegenban <tadegenban@gmail.com>',
+    LICENSE => 'perl',
+    MIN_PERL_VERSION => '5.008001',
+    test => { TESTS => 't/*.t' },
+    postamble => {
+        inline => {
+            module => 'Text::Levenshtein::Inline',
+        },
     },
-  },
 );
+
